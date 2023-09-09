4163a89ccd10d28d9ac6a671e981e62e37d1ba0a
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-09-09 23:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let inpName = document.getElementById('item-name-input');
+let inpPrice = document.getElementById('item-price-input');
+let h3 = document.getElementById('hh');
+let itemList = [{item:'',price:0}]
+let addbtn = document.getElementById('add');
+addbtn.addEventListener('click', function() {
+	itemList.item.push(inpName.value);
+	itemList.price.push(Number(inpPrice.value));
+	h3.innerText = itemList[0].item;
+})
