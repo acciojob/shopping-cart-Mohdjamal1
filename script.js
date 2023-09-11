e8337de2ba09b55ca69b1524e767ac3ab164d353
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-09-11 10:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 let inpName = document.getElementById('item-name-input');
 let inpPrice = document.getElementById('item-price-input');
 let tbody = document.getElementById('tbody');
+let total = document.getElementById('priceT');
 let itemList = [];
+let totalPrice = 0;
 let addbtn = document.getElementById('add');
 addbtn.addEventListener('click', ()=> {
+	if(inpName.value !== "" && inpPrice !== 0){
 	let obj = {item:`${inpName.value}`,price:inpPrice.value}
+		totalPrice += Number(inpPrice.value);
+		total.innerText = `Total: ${totalPrice}`;
 	itemList.push(obj);
-	inpName.value ="";
+	inpName.value =""; 
 	inpPrice.value ="";
 	tbody.innerHTML = '';
 	itemList.forEach((emp)=>{
 	let tr = document.createElement('tr');
-	tr.innerHTML = `<td>${emp.item}</td>
-			<td>${emp.price}</td>`
+	tr.innerHTML = `<td id='item'>${emp.item}</td>
+			<td id='price'>${emp.price}</td>`
 		tbody.append(tr);
 	})
+	}
 })
