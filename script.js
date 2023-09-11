8b3ec99988d55f94ef0adfd404ad7034dd080003
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-09-11 11:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 //your code here
 let inpName = document.getElementById('item-name-input');
-let inpPrice = document.getElementById('item-qty-input');
+let inpQty = document.getElementById('item-qty-input');
+let inpPrice = document.getElementById('item-price-input');
 let tbody = document.getElementById('tbody');
 let total = document.getElementById('priceT');
 let itemList = [];
 let totalPrice = 0;
 let addbtn = document.getElementById('add');
 addbtn.addEventListener('click', ()=> {
-	if(inpName.value !== "" && inpPrice !== 0){
-	let obj = {item:`${inpName.value}`,price:inpPrice.value}
+	if(inpName.value !== "" && inpQty !== ""){
+	let obj = {item:`${inpName.value}`,qty:`${inpQty.value}`,price:inpPrice.value}
 		totalPrice += Number(inpPrice.value);
-		total.innerText = `Total: ${totalPrice}`;
+		total.innerText = `Total Price: ${totalPrice}`;
 	itemList.push(obj);
 	inpName.value =""; 
+	inpQty.value =""; 
 	inpPrice.value ="";
 	tbody.innerHTML = '';
 	itemList.forEach((emp)=>{
 	let tr = document.createElement('tr');
 	tr.innerHTML = `<td id='item'>${emp.item}</td>
+			 <td id='qtyy'>${emp.qty}</td>
 			<td id='price'>${emp.price}</td>`
 		tbody.append(tr);
 	})
